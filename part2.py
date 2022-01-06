8bf6df944faafbb769ad9ff97b704d51a335f93a
Cal Chen <94643822+calchen1@users.noreply.github.com>
2022-01-06 12:47:36 +0000

    part 1: fixed the program part 2: wrote a program that prints the sum of the first n positive cubes of the input n part3: wrote a program that prints a rectangle of asterisks with the dimensions the user enters part4: wrote a program

--- part2.py
+++ part2.py
@@ -7,4 +7,11 @@ Write a program that asks the user to enter a positive integer n. The program wi
 For example, if the user types in 4, the program should print 100 (since 1^3 + 2^3 + 3^3 + 4^3 = 100).
 """
 
-#write your code here
+n = int(input("Enter a number: "))
+
+sum = 0 
+
+for i in range (1, n+1):
+  sum += i ** 3 
+
+print (sum)
